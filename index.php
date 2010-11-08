551c452cb94c6c52a5269072f1cbffb758db2926
prolabs <prolabs@prolabs.com.br>
2010-11-08 06:22:52 -0800

    index.php adjust

--- index.php
+++ index.php
@@ -8,7 +8,8 @@
 | By default CI runs with error reporting set to ALL.  For security
 | reasons you are encouraged to change this when your site goes live.
 | For more info visit:  http://www.php.net/error_reporting
-|
+| 
+| Public pull request testing...
 */
 	error_reporting(E_ALL);
 	
